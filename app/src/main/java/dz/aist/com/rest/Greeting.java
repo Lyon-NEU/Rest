38690808394f48cd8dd2943dae8c275c0a0cb3e9
Lyon-NEU <wanlplab@gmail.com>
2016-05-13 15:13:36 +0800

    first commit

---
+++ app/src/main/java/dz/aist/com/rest/Greeting.java
@@ -0,0 +1,16 @@
+package dz.aist.com.rest;
+
+/**
+ * Created by Wang on 2016/5/12.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId(){
+        return id;
+    }
+    public String getContent(){
+        return content;
+    }
+}
